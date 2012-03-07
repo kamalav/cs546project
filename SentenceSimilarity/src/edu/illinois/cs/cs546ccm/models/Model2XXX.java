1fee9575df7d587c2c0f65df4b117c9b70145736
threept14guy <threept14guy@gmail.com>
2012-03-07 03:22:06 +0000

    Added stub methods for the separate view similarity metrics

--- SentenceSimilarity/src/edu/illinois/cs/cs546ccm/models/Model2XXX.java
+++ SentenceSimilarity/src/edu/illinois/cs/cs546ccm/models/Model2XXX.java
@@ -10,11 +10,38 @@ public class Model2XXX extends Model {
 
 	@Override
 	public double similarity(Corpus corpus, int line) {
-		// TODO implement this method
+		double score1 = score1();
+        double score2 = score2();
+        double score3 = score3();
+        double score4 = score4();
 		return -1;
 	}
 
-	@Override
+	private double score4() {
+        // TODO Auto-generated method stub
+	    // Zhijin's method
+        return 0;
+    }
+
+    private double score3() {
+        // TODO Auto-generated method stub
+        // Cedar's method
+        return 0;
+    }
+
+    private double score2() {
+        // TODO Auto-generated method stub
+        // Guihua's method
+        return 0;
+    }
+
+    private double score1() {
+        // TODO Auto-generated method stub
+        // Ryan's method
+        return 0;
+    }
+
+    @Override
 	public int confidence(Corpus corpus, int line) {
 		// TODO implement this method
 		return -1;
